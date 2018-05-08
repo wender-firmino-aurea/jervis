a1d0dc48029fb132f4e708d2693196209c1dcf9d
Paulo Kinjo <paulokinjo@hotmail.com>
2018-05-08 14:44:38 -0400

    change script

--- assets/install_agents.sh
+++ assets/install_agents.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Accessing jenkins remotely..."
+env
 echo $JENKINS_API_TOKEN_TEST
 curl --user 'admin:01f27f492862850a529084c3f71d6428' --data-urlencode "script=$(< /opt/my_jenkins_home/workspace/_jervis_generator/jobs/install_agents.groovy)" http://localhost:8080/scriptText
