0eea79f6df9626c47aa83c918bf53cfd0988a319
Paulo Kinjo <paulokinjo@hotmail.com>
2018-05-08 11:58:28 -0400

    change script

--- assets/install_agents.sh
+++ assets/install_agents.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Accessing jenkins remotely..."
-
+ls -al
+pwd
 curl --user 'admin:01f27f492862850a529084c3f71d6428' --data-urlencode "script=$(< ./install_agents.groovy)" http://localhost:8080/scriptText
