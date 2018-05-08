32b983d5dee94d9e14df26147bf245855a1a5c4c
Paulo Kinjo <paulokinjo@hotmail.com>
2018-05-08 15:14:16 -0400

    change script

--- assets/install_agents.sh
+++ assets/install_agents.sh
@@ -5,4 +5,4 @@ echo $JENKINS_API_TOKEN_USER
 echo $JENKINS_API_TOKEN_PASSWORD
 echo $JENKINS_SERVER_URL
 
-curl --user '$JENKINS_API_TOKEN_USER:$JENKINS_API_TOKEN_PASSWORD' --data-urlencode "script=$(< /opt/my_jenkins_home/workspace/_jervis_generator/jobs/install_agents.groovy)" http://localhost:8080/scriptText
+curl --user """$JENKINS_API_TOKEN_USER:$JENKINS_API_TOKEN_PASSWORD""" --data-urlencode "script=$(< /opt/my_jenkins_home/workspace/_jervis_generator/jobs/install_agents.groovy)" http://localhost:8080/scriptText
