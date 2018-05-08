1935c6ad3497b7257109de3f59516996e2362f8b
Paulo Kinjo <paulokinjo@hotmail.com>
2018-05-08 11:57:18 -0400

    change script

--- assets/install_agents.sh
+++ assets/install_agents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Accessing jenkins remotely..."
 
-curl --user 'admin:01f27f492862850a529084c3f71d6428' --data-urlencode "script=$(< ../jobs/setup_docker_slave.groovy)" http://localhost:8080/scriptText
+curl --user 'admin:01f27f492862850a529084c3f71d6428' --data-urlencode "script=$(< ./install_agents.groovy)" http://localhost:8080/scriptText
