b8fc69fa11076a7952a6096c9492d6ca781e0675
Paulo Kinjo <paulokinjo@hotmail.com>
2018-05-08 11:34:42 -0400

    change script

--- jobs/firstjob_dsl.groovy
+++ jobs/firstjob_dsl.groovy
@@ -71,7 +71,7 @@ println 'Installing agents...';
 //   exec(it)
 // }
 
-def command = 'curl --user "admin:47098414b3fa40f3a994befc1ea55ca3" --data-urlencode "script=$(< ./install_agents.groovy)" http://172.17.0.1:8080/scriptText'
+def command = "curl --user \"admin:01f27f492862850a529084c3f71d6428\" --data-urlencode \"script=$(< ./install_agents.groovy)\" http://172.17.0.1:8080/scriptText"
 def proc = command.execute()
 proc.waitFor()              
 
