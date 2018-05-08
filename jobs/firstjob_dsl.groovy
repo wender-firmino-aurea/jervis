1ed83b171e5173ebabe22f81204a707780e7c623
Paulo Kinjo <paulokinjo@hotmail.com>
2018-05-08 09:08:12 -0400

    change script

--- jobs/firstjob_dsl.groovy
+++ jobs/firstjob_dsl.groovy
@@ -64,7 +64,7 @@ def exec(cmd) {
 }
 
 [
-  "curl --user 'admin:dd27742ddefca924dfac4d0d4f1354ef' --data-urlencode 'script=$(< ../jobs/setup_docker_slave.groovy)' http://localhost:8080/scriptText"
+  'curl --user "admin:dd27742ddefca924dfac4d0d4f1354ef" --data-urlencode "script=$(< ./install_agents.groovy)" http://localhost:8080/scriptText'
 ].each {
   exec(it)
 }
